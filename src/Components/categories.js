0b3811d915c3e925d46fbf13f5bd2c08967c5aa0
shubh242 <64204436+shubh242@users.noreply.github.com>
2024-04-08 11:52:00 +0000

    Fixing Homepage Alignment issue

--- src/Components/categories.js
+++ src/Components/categories.js
@@ -1,43 +1,50 @@
-import { Button, ButtonGroup } from '@mui/material';
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Button, ButtonGroup, Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function CategoriesComponent({updateCategoryData}) {
+export default function CategoriesComponent({ updateCategoryData }) {
+  const url = `https://kitsu.io/api/edge/categories?sort=-totalMediaCount`;
+  const [categories, setCategories] = useState([]);
 
-    const url = `https://kitsu.io/api/edge/categories?sort=-totalMediaCount`;
-    const [categories, setCategories] = useState([]);
+  const categoryData = async () => {
+    let category = await fetch(url);
+    category = await category.json();
+    setCategories(category.data);
+  };
 
+  useEffect(() => {
+    // eslint-disable-next-line
+    categoryData();
+  }, []);
 
-    const categoryData = async () => {
-        let category = await fetch(url);
-        category = await category.json();
-        setCategories(category.data);
-    }
+  const handleClick = (e, title) => {
+    updateCategoryData(title);
+  };
 
-    useEffect(() => {
-        // eslint-disable-next-line
-        categoryData();
-    }, []);
-
-    const handleClick = (e, title) => {
-        updateCategoryData(title);
-    }
-
-    return (
-        <div>
-            <ButtonGroup variant="text" aria-label="Basic button group">
-                {
-                    categories.map((category) => {
-                        return (
-                            <div key={category.id} >
-                                <Link to={`/anime/category/${category.id}`}>
-                                    <Button onClick={event => handleClick(event, category.attributes.title)}>{category.attributes.title}</Button>
-                                </Link>
-                            </div>
-                        )
-                    })
-                }
-            </ButtonGroup>
-        </div >
-    );
+  return (
+    <div>
+      {/* <Grid></Grid> */}
+      <ButtonGroup
+        variant="text"
+        aria-label="Basic button group"
+        sx={{ flexWrap: "wrap" }}
+      >
+        {categories.map((category) => {
+          return (
+            <div key={category.id}>
+              <Link to={`/anime/category/${category.id}`}>
+                <Button
+                  onClick={(event) =>
+                    handleClick(event, category.attributes.title)
+                  }
+                >
+                  {category.attributes.title}
+                </Button>
+              </Link>
+            </div>
+          );
+        })}
+      </ButtonGroup>
+    </div>
+  );
 }
--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,29 +1,37 @@
-import React, { useState } from 'react'
-import '../styles.css'
-import DisplayTileComponent from '../Components/displayTileComponent';
-import Sidenav from '../Components/sidenav';
-import { Box } from '@mui/material';
+import React, { useState } from "react";
+import "../styles.css";
+import DisplayTileComponent from "../Components/displayTileComponent";
+import Sidenav from "../Components/sidenav";
+import { Box } from "@mui/material";
 
 export default function Home() {
-    const [searchString, setSearchString] = useState('');
-    const [animeCollection, setAnimeCollection] = useState([]);
+  const [searchString, setSearchString] = useState("");
+  const [animeCollection, setAnimeCollection] = useState([]);
 
-    const updateSearchString = (inputString) => {
-        setSearchString(inputString);
-    }
+  const updateSearchString = (inputString) => {
+    setSearchString(inputString);
+  };
 
-    const updateAnimeCollection = (collection) => {
-        setAnimeCollection([...animeCollection, collection.animeFetchData], () => {
-            console.log(animeCollection);
-        });
-    }
+  const updateAnimeCollection = (collection) => {
+    setAnimeCollection([...animeCollection, collection.animeFetchData], () => {
+      console.log(animeCollection);
+    });
+  };
 
-    return (
-        <div>
-            <Sidenav searchString={searchString} updateSearchString={updateSearchString} />
-            <Box component="main" sx={{ marginTop: 10}}>
-                <DisplayTileComponent searchString={searchString} animeCollection={animeCollection} updateAnimeCollection={updateAnimeCollection} updateSearchString={updateSearchString} />
-            </Box>
-        </div>
-    );
+  return (
+    <div>
+      <Sidenav
+        searchString={searchString}
+        updateSearchString={updateSearchString}
+      />
+      <Box component="main" sx={{ marginTop: 10, marginLeft: 10 }}>
+        <DisplayTileComponent
+          searchString={searchString}
+          animeCollection={animeCollection}
+          updateAnimeCollection={updateAnimeCollection}
+          updateSearchString={updateSearchString}
+        />
+      </Box>
+    </div>
+  );
 }
